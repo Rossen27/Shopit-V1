b3667677c1f58cc344511b9ba5fde51286885d46
2200517 <rossenlin@syspower.com.tw>
2024-03-27 15:32:02 +0800

    Refactor OAuth component to use Google login API

--- frontend/src/components/layout/OAuth.jsx
+++ frontend/src/components/layout/OAuth.jsx
@@ -4,29 +4,24 @@ import { app } from '../../firebase';
 import { useDispatch } from 'react-redux';
 import { signInSuccess } from '../../redux/features/userSlice';
 import { useNavigate } from 'react-router-dom';
-
+import { useGoogleLoginMutation } from "../../redux/api/authApi";
 
 export default function OAuth() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [googleLogin] = useGoogleLoginMutation();
+
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const auth = getAuth(app);
       const result = await signInWithPopup(auth, provider);
       // 將登入資料傳送給後端
-      const res = await fetch('/api/v1/google', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          name: result.user.displayName,
-          email: result.user.email,
-          photo: result.user.photoURL,
-        }),
+      const { data } = await googleLogin({ // 呼叫後端的 Google 登入 API
+        name: result.user.displayName,
+        email: result.user.email,
+        photo: result.user.photoURL,
       });
-      const data = await res.json();
       dispatch(signInSuccess(data));
       navigate('/');
     } catch (error) {
