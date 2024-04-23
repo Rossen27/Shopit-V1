34497b5d14cac51c83278315e88f8abd20c46f48
RossenLin <jetso1127@gmail.com>
2024-04-23 16:28:38 +0800

    Add admin product routes and controllers

--- frontend/src/components/routes/adminRoutes.jsx
+++ frontend/src/components/routes/adminRoutes.jsx
@@ -1,6 +1,8 @@
 import { Route } from "react-router-dom";
 import ProtectedRoute from "../auth/ProtectedRoute.jsx";
 import Dashboard from "../admin/Dashboard.jsx";
+import ListProducts from "../admin/ListProducts.jsx";
+import NewProduct from "../admin/NewProduct.jsx";
 
 const adminRoutes = () => {
   return (
@@ -10,6 +12,22 @@ const adminRoutes = () => {
         element={
           <ProtectedRoute admin={true}>
             <Dashboard />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/products"
+        element={
+          <ProtectedRoute admin={true}>
+            <ListProducts />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin/product/new"
+        element={
+          <ProtectedRoute admin={true}>
+            <NewProduct />
           </ProtectedRoute>
         }
       />
