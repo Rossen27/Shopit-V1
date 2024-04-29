53d1b6ec0f9c7f853d5972c3ce53e4b49726cd74
RossenLin <jetso1127@gmail.com>
2024-04-29 17:47:52 +0800

    Update configuration files and remove unused environment variables

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,24 +6,24 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig({
   plugins: [react(), svgr()],
   server: {
-    port: 8080,
+    // port: 8080,
     proxy: {
       "/api": {
-        target: "http://localhost:10000", // 這裡是後端的 port
+        target: "http://localhost:3000", // 這裡是後端的 port
         secure: false, // 若後端是 https 則設為 true
       },
     },
   },
-  // // 建構配置
-  // build: {
-  //   // 生成環境時將環境變數注入到代碼中
-  //   sourcemap: true,
-  //   rollupOptions: {
-  //     output: {
-  //       inlineDynamicImports: true,
-  //     },
-  //   },
-  // },
+  // 建構配置
+  build: {
+    // 生成環境時將環境變數注入到代碼中
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+  },
   extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx"],
   resolve: {
     mainFields: [],
