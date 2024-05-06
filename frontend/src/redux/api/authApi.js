0b5df211473eaad5c300ec6514c223a1ec7d0dd4
RossenLin <97585139+Rossen27@users.noreply.github.com>
2024-05-06 15:34:39 +0800

    Update authApi.js

--- frontend/src/redux/api/authApi.js
+++ frontend/src/redux/api/authApi.js
@@ -15,7 +15,7 @@ export const authApi = createApi({
       },
       async onQueryStarted(args, { dispatch, queryFulfilled }) {
         try {
-          await queryFulfilled; // 等待 query 完成
+          await queryFulfilled;
           await dispatch(userApi.endpoints.getMe.initiate(null));
         } catch (error) {
           console.log(error);
@@ -42,25 +42,8 @@ export const authApi = createApi({
     logout: builder.query({
       query: () => "/logout",
     }),
-    googleLogin: builder.mutation({
-      query(body) {
-        return {
-          url: "/google",
-          method: "POST",
-          body,
-        };
-      },
-      async onQueryStarted(args, { dispatch, queryFulfilled }) {
-        try {
-          await queryFulfilled;
-          await dispatch(userApi.endpoints.getMe.initiate(null));
-        } catch (error) {
-          console.log(error);
-        }
-      },
-    }),
   }),
 });
 
-export const { useLoginMutation, useRegisterMutation, useLazyLogoutQuery, useGoogleLoginMutation } =
+export const { useLoginMutation, useRegisterMutation, useLazyLogoutQuery } =
   authApi;
