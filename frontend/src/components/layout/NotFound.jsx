15b0b91a79944e2c2092ddebc00f66a73dba077d
RossenLin <97585139+Rossen27@users.noreply.github.com>
2024-05-06 16:05:25 +0800

    feat: Add react-router-dom to NotFound component
    Update NotFound component to import and use the `Link` component from `react-router-dom` instead of using an anchor tag for the "Go Back Home" link. This ensures proper navigation within the application.
    
    chore: Update product details icons in ProductDetails component
    
    Replace anchor tags with div elements for the parking, chair, and info icons in the ProductDetails component. This improves accessibility and ensures proper rendering of the icons.
    
    chore: Update routing configuration and target URL in vite.config.js
    
    Update the routing configuration and target URL in the vite.config.js file. This change is necessary to reflect the updated routing structure and ensure proper navigation within the application.

--- frontend/src/components/layout/NotFound.jsx
+++ frontend/src/components/layout/NotFound.jsx
@@ -1,4 +1,6 @@
 /* eslint-disable react/no-unescaped-entities */
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
   return (
     <div className="grid h-screen place-content-center bg-white px-4">
@@ -6,12 +8,12 @@ const NotFound = () => {
         <h1 className="text-9xl font-black text-gray-200">404</h1>
         <h1 className="uppercase tracking-widest text-gray-500">404 | Not Found</h1>
 
-        <a
-          href="/"
+        <Link
+          to="/"
           className="mt-6 inline-block rounded bg-emerald-600 px-5 py-3 text-sm font-medium text-white hover:bg-emerald-700 focus:outline-none focus:ring"
         >
           Go Back Home
-        </a>
+        </Link>
       </div>
     </div>
   );
